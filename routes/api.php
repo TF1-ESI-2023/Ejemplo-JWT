9c448d6e6ea4cf10314740e50f89e383ad32cc8a
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2022-10-31 00:55:59 -0300

    JWT Working, I guess

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\controlador;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -17,9 +16,16 @@ use App\Http\Controllers\controlador;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh',[AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
 });
-
-
-
